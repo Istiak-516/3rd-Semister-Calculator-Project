190f085c88e4a14f06f00c143f33be573f8790ef
Istiak-Ahmed <aistiak559@gmail.com>
2024-12-27 00:06:39 +0600

    Changed

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,8 @@
+
+import java.awt.Frame;
+
 public class Calculator{
-    Frame f;
+    Frame f=new Frame("Calculator");
     public static void main(String[] args) {
         System.out.println("Testing...1");
         System.out.println("New line");
